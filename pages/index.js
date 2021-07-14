c34b1d2337beb3e228ec1820bed0091491ef9262
Roger Katona <roger.katona@gmail.com>
2021-07-14 12:32:19 -0400

    Removed Image tag

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
-import Image from "next/image";
+
 
 
 export default function Home() {
@@ -11,13 +11,7 @@ export default function Home() {
       </Head>
 
       <div className="flex flex-col items-center justify-center w-full flex-1 p-20 text-center bg-primaryLight h-screen">
-          <Image
-              priority
-              src="/images/logo-bp.png"
-              height={473}
-              width={1281}
-              alt={'logo'}
-          />
+          <img src="images/logo-bp.png" alt="Blue Prospect Logo" width={1281} height={473} />
         <p className="mt-3 text-xl px-0 w-2/3 mt-24 text-white">
           Our site will probably be up very soon!  If you are interested in knowing what is happening at this very moment, picture a dude on the side of the road with hood up, smoke billowing, head scratching.
         </p>
