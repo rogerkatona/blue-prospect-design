0514ca31d2fca5e1adbcef1a6a04fc704228e35d
Roger Katona <roger.katona@gmail.com>
2021-07-16 17:22:19 -0400

    Updated version of aws-sdk

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,8 +3,10 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
-    "aws_cognito_identity_pool_id": "us-east-1:e136ad44-a696-484c-9c11-9e97b6ddbaa7",
+    "aws_cognito_identity_pool_id": "us-east-1:a067fbcd-0e1c-4356-a2b8-56f44a0e5818",
     "aws_cognito_region": "us-east-1",
+    "aws_user_pools_id": "us-east-1_dCmFyjerf",
+    "aws_user_pools_web_client_id": "1f0ce20166sat38slk8ptjvbjn",
     "oauth": {},
     "aws_mobile_analytics_app_id": "d0599af832fd48b2992ef9766fa8162e",
     "aws_mobile_analytics_app_region": "us-east-1"
